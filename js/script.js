d443b1b1681f5e314ea752d335a583602e065295
Artur Trąpczyński <trapczynski.a@gmail.com>
2020-11-22 15:32:36 +0100

    Add generateTags function - select articles, tagsWrapper, tags, loop

--- js/script.js
+++ js/script.js
@@ -1,56 +1,52 @@
 'use strict';
 
 const activeClass = 'active';
+const optArticleSelector = '.post';
+const optTitleSelector = '.post-title';
+const optTitleListSelector = '.titles';
+const optLinkSelector = 'titles a';
+const optArticleTagsSelector ='.post-tags .list';
 
-const titleClickHandler = function(event) {
+const titleClickHandler = function (event) {
   event.preventDefault();
+
   const clickedElement = this;
-  const activeLinks = document.querySelectorAll('.titles a.' + activeClass);
-  removeClassFromList(activeLinks);
-  addClassToElement(clickedElement);
-
-  const activeArticles = document.querySelectorAll('.posts article.' + activeClass);
-
-  removeClassFromList(activeArticles);
-
+  const activeLinks = document.querySelectorAll(optLinkSelector +'.'+ activeClass);
   const href = clickedElement.getAttribute('href');
   const selectedArticle = document.querySelector(href);
+  const activeArticles = document.querySelectorAll('.posts article.' + activeClass);
 
+  removeClassFromList(activeLinks);
+  addClassToElement(clickedElement);
+  removeClassFromList(activeArticles);
   addClassToElement(selectedArticle);
 
   function addClassToElement(element, className = activeClass) {
     element.classList.add(className);
   }
-  function removeClassFromList(list, className = activeClass){
-    for(let element of list){
-      element.classList.remove(className);
-    }
+  function removeClassFromList(list, className = activeClass) {
+    for (let element of list) {
+      element.classList.remove(className);    }
   }
 };
 
-const optArticleSelector = '.post';
-const optTitleSelector = '.post-title';
-const optTitleListSelector = '.titles';
-
-function removeElement(element){
+const removeElement = function(element) {
   element.innerHTML = '';
-}
+};
 
-const generateTitleLinks = function(){
+const generateTitleLinks = function () {
   const titleList = document.querySelector(optTitleListSelector);
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = '';
 
   removeElement(titleList);
 
-  const articles = document.querySelectorAll(optArticleSelector);
-
-  let html ='';
-
-  for(let article of articles){
+  for (let article of articles) {
     const articleId = article.getAttribute('id');
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
     const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
     html = html + linkHTML;
-
   }
 
   titleList.innerHTML = html;
@@ -64,3 +60,15 @@ for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
 
+const generateTags = function(){
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html ='';
+
+  for(let article of articles){
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
+    const tags = article.getAttribute('data-tags');
+  }
+
+
+};
